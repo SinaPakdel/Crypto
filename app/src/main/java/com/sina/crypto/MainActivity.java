5db2420c841e7a6aed0cf421c20b0223b14a1bb2
sina <sina.tabrizyian@gmail.com>
2023-08-25 15:53:47 +0330

    impl navHostFragment with smoothBottomNavigation

--- app/src/main/java/com/sina/crypto/MainActivity.java
+++ app/src/main/java/com/sina/crypto/MainActivity.java
@@ -1,18 +1,35 @@
 package com.sina.crypto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.PopupMenu;
 
 import com.sina.crypto.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    NavHostFragment navHostFragment;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        navController = navHostFragment.getNavController();
+        setupBottomNavMenu();
+
+    }
+    private void setupBottomNavMenu() {
+        PopupMenu popupMenu = new PopupMenu(this, null);
+        popupMenu.inflate(R.menu.home_menu);
+        Menu menu = popupMenu.getMenu();
+
+        binding.smoothBottomBar.setupWithNavController(menu, navController);
     }
 }
---
+++ app/src/main/java/com/sina/crypto/fragment/wtchlist/WatchlistFragment.java
@@ -0,0 +1,20 @@
+package com.sina.crypto.fragment.wtchlist;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sina.crypto.R;
+
+public class WatchlistFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_watchlist, container, false);
+    }
+}
